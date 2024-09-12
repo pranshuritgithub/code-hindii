568120fa32af3d4df73a06e1782c045badc05180
pranshuritgithub <prashantjais004@gmail.com>
2024-09-12 05:08:39 +0000

    data_type

---
+++ js/data_type.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+let name= "Prashant"   // string
+let age= 20            // int
+let isloggedin= false  // boolean
+let state;             // undefined
+
+//data types
+//number => 2 to power 53
+// bigint used as in also in trading
+// string => ""
+// boolean => true/false
+// null=> standalone valua
+// undefined =>
+// symbol => unique   
+
+// object
