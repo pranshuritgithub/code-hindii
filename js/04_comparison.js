3039eb3f6248e7789bef9474566ffe5f5f887b3b
pranshuritgithub <prashantjais004@gmail.com>
2024-09-13 17:45:42 +0000

    04_comparison

---
+++ js/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);       // null is an empty value
+console.log(null == 0);
+console.log(null >= 0);
+
+// strict check means ( === )
+
+console.log("2" === 2);
+console.log(2 === 2);
+
+
+
+
