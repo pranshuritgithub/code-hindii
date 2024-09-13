5667885c9593c16d1c8d146aab434cb8621c202d
pranshuritgithub <prashantjais004@gmail.com>
2024-09-13 17:25:42 +0000

    03_conversionOperation

--- js/03_conversionOperation.js
+++ js/03_conversionOperation.js
@@ -37,4 +37,27 @@ console.log(negValue);
 //console.log(2/3);
 //console.log(2%3);
 
+let str1 = "hello"
+let str2 = " Prashant"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 +2);
+console.log(1+ 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// study prefix and postfix in MDN
+
+
+
+
+
 
