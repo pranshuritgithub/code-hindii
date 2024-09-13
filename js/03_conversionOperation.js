8e9d71afbaff6dfaafbb820e3a53b810a2c5149c
pranshuritgithub <prashantjais004@gmail.com>
2024-09-13 08:31:41 +0000

    03_conversionOperation

--- js/03_conversionOperation.js
+++ js/03_conversionOperation.js
@@ -1,5 +1,40 @@
+
+//       CONVERSION
+
 let score=33
 let score1=null
 console.log(typeof "score");
 console.log(typeof(score1));
+
+//"33" => 33
+//"33abc" => NaN (not a number)
+// true => 1; false =>0
+
+let isloggedin = ""
+
+let booleanisloggedin = Boolean(isloggedin)
+console.log(booleanisloggedin);
+
+// 1=> true; 0=> false
+// "" => false
+//"Prashant" => true
+
+let someNumber = 45
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber); 
+
+//          OPERATION
+
+let value =3
+let negValue=-value
+console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3);
+
 
